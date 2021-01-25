82d89ef2ac0402aa628c11215e23bbc588feb87e
Junren Deng <junren.deng@gmail.com>
2021-01-25 14:02:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo ::set-output name=debug::ARGS: \("$1", "$2", "$3", "$4"\)
 
-./query_issues.py -t "$1" -r "$2" -a "$3" -s "$4"
+# TODO GITHUB_REPOSITORY passed by docker -e
+/query_issues.py -t "$1" -r "$GITHUB_REPOSITORY" -a "$3" -s "$4"
